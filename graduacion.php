68f3ad76e03d7500d0527f4348fd4a8b16623360
Yonatan936 <enriquito936@gmail.com>
2019-10-23 10:51:08 -0500

    php

--- graduacion.php
+++ graduacion.php
@@ -3,21 +3,20 @@
 <head>
     <meta charset="UTF-8">
     <title>Login</title>
-    <link ref= "stylessheet" href="css/main.css"/>
+    <link rel="stylesheet" href="css/main.css">
 </head>
 <body>
-
 <section class ="contrainer">
-    <section class ="row">
+<section class ="row">
         <div class="col-md6">
             <form action ="resultado.php" method="GET">
               <div class="form-group">
-                <label for ="">Usuario</label>
-                <input type ="text" class="form control" name="usuario">
+                    <label for ="">Usuario</label>
+                     <input type ="text" class="form control" name="usuario">
                 </div>
                 <div class="form-group">
-                <label for ="">password</label>
-                <input type ="password" class="form control" name="password">
+                    <label for ="">password</label>
+                    <input type ="password" class="form control" name="password">
                 </div>
                 <button class="btn_primary">Enviar Datos </button>
 
--- resultado.php
+++ resultado.php
@@ -3,18 +3,20 @@
 <head>
     <meta charset="UTF-8">
     <title>Resultado</title>
-    <link ref= "stylessheet" href="css/main.css"/>
+    <link rel="stylesheet" href="css/main.css">
 </head>
 <body>
     
-    <p class="text-info">
+    <p  
+        class="text-info">
         <?php 
          $usuario=$_Get["usuario"];
             echo $usuario;
          ?>
 
     </p>
-    <p class="text-info">
+    <p 
+        class="text-info">
 
         <?php 
             $password=$_Get["password"];
