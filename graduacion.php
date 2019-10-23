95a0f88534b172706624dc9d009ba8e7fd2baf7c
Yonatan936 <enriquito936@gmail.com>
2019-10-23 17:55:20 -0500

    php

--- graduacion.php
+++ graduacion.php
@@ -4,25 +4,54 @@
     <meta charset="UTF-8">
     <title>Login</title>
     <link rel="stylesheet" href="css/main.css">
+    <link rel="stylesheet" href="css/bootstrap.css">
+    <link rel="stylesheet" href="css/all.min.css">
+    <script src="js/jquery-3.4.1.min.js"></script>
+    <script src="js/bootstrap.js"></script>
 </head>
 <body>
-<section class ="contrainer">
-<section class ="row">
-        <div class="col-md-6">
+<button class="btn btn-primary"  
+  data-target="#mimodal"    data-role="modal"  
+  data-toggle="modal">  Log In</button>
+      
+    <form action ="resultado.php" method="GET">
+    <div class="modal fade" id="mimodal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel"
+    aria-hidden="true">
+    <div class="modal-dialog" role="document">
+        <div class="modal-content">
+            <div class="modal-header">
+                <h5 class="modal-title" id="exampleModalLabel">Log In</h5>
+                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                <span aria-hidden="true">&times;</span>
+                </button>
+            </div>
+            <div class="modal-body">
+            <section class ="row">
+            <div class="col-md-6">
             <form action ="resultado.php" method="GET">
-              <div class="form-group">
-                    <label for ="">Usuario</label>
-                     <input type ="text" class="form control" name="usuario">
+                        <div class="form-group ">
+                            <label for="nombre">Nombre</label>
+                            <input type="text" class="form-control" name="usuario">
+                        </div>
+                        <div class="form-group">
+                            <label for="password">Password</label>
+                            <input type="password" class="form-control" name="password">
+                        </div>
+                    </form>
                 </div>
-                <div class="form-group">
-                    <label for ="">password</label>
-                    <input type ="password" class="form control" name="password">
-                </div>
-                <a class="btn btn-primary btn-lg" href="#" role="button">Enviar Datos</a>
-            </form>
+            </section>
+            </div>
+            <div class="modal-footer">
+                <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+                <button class="btn btn-outline-primary btn-block">
+                        <i class="fas "></i> Enviar Datos</button>
+                        
+                        </div>
+       
+            </div>
         </div>
-</section>
-</section>
-    
+    </div>
+</div>
+
 </body>
 </html>
--- resultado.php
+++ resultado.php
@@ -6,7 +6,8 @@
     <link rel="stylesheet" href="css/main.css">
 </head>
 <body>
-    
+    <div class="col-md-6">
+            <form action ="graduacion.php" method="GET">
     <p class="text-info">
         <?php 
             $usuario=$_GET["usuario"];
@@ -15,11 +16,14 @@
 
     </p>
     <p class="text-info">
-    
+
         <?php 
             $password=$_GET["password"];
             echo $password;
         ?>
     </p>
+    <button class="btn btn-outline-primary btn-block">
+                        <i class="fas "></i> SALIR</button>
+
 </body>
 </html>
