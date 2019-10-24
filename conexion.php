55950053695d4312664f160e190e9a1d3e739945
Yonatan936 <enriquito936@gmail.com>
2019-10-24 08:39:51 -0500

    php

--- conexion.php
+++ conexion.php
@@ -1,5 +1,10 @@
 <?php
 $conexionBD= new mysqli ("localhost"," root","admin1234","graduacion4b");
 
-
+if ($conexionBD->connect_error){
+    die("Error en la conexion".$conexionBD->connect_error);
+}
+else {
+    echo"Conexion exitosa a la base de datos";
+}
 ?>
